a68b605bd285c514d6ab1dfd50be332971c65988
David Star Kim <ollehstar@gmail.com>
2022-12-24 20:37:42 +0900

    221223-221223 공부

---
+++ Daily/d_221224.java
@@ -0,0 +1,26 @@
+package Daily;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class d_221224 {
+    /*
+     * Stack은 이 책에서는 직접 구현하는 것...
+     * isEmpty 까지 구현하는 것 보고 놀람!
+     * 
+     * Queue도 peek나 clear도 구현하는군요.
+     * 기본적으로 제공하나, 공부 목적으로는 읽어볼만 하다고 생각.
+     * 
+     * 링 버퍼의 개념도 제공하는데, insert가 많더라도 너무 오래 된 것은 버리는 개념! (최대 n개로 유지)
+     * 
+     */
+
+      public static void main(String[] args) {
+        Stack stack = new Stack<>(); //스택은 뭐... 코딩테스트에서 맨날천날 쓰는 걔입니다... LIFO
+        Queue <Integer> queue = new LinkedList<>(); //이걸루 쓰면 됨! 자바 큐는 링크드리스트로 구현함! FIFO
+        
+
+
+      }
+}
